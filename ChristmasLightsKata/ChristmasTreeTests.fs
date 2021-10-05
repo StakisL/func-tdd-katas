afbb78c1e202e75168035c32318a68f350fc3fc6
stanislav <s.lebel@dodopizza.com>
2021-10-05 18:03:21 +0300

    WIP : ChristmasTree should turn on in first row on tree.

--- ChristmasLightsKata/ChristmasTreeTests.fs
+++ ChristmasLightsKata/ChristmasTreeTests.fs
@@ -26,3 +26,19 @@ type ChristmasTreeTests() =
         //assert
         for i in 0 .. turnedOnTree.Length - 1 do
             Assert.IsTrue(turnedOnTree.[i] = true)
+            
+    [<Test>]
+    member this.ChristmasTree_ShouldTurnOnFirstRowOnTree () =
+        //arrange
+        let christmasTree = ChristmasTree(1000,1000)
+        
+        //act
+        let result = christmasTree.TurnOnFirstRow
+        
+        //assert
+        for i in 0 .. 999 do
+            Assert.IsTrue(result.[i] = true)
+        
+    //todo for next kata time
+    //make special type for tree
+    //split tree type with functions
