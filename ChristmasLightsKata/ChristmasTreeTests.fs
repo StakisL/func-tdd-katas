e4043e0855ccef3053ef49856e1dce26f1e65831
stanislav <s.lebel@dodopizza.com>
2021-10-05 17:53:27 +0300

    Add test TurnOnAll lights in tree and small refactoring.

--- ChristmasLightsKata/ChristmasTreeTests.fs
+++ ChristmasLightsKata/ChristmasTreeTests.fs
@@ -10,3 +10,10 @@ type ChristmasTreeTests() =
     member this.ChristmasTree_ShouldCreateDisabledChristmasTree () =
         let christmasTree = ChristmasTree(1000,1000)
         Assert.IsTrue(christmasTree.IsLightsOff)
+
+    [<Test>]
+    member this.ChristmasTree_ShouldTurnOnEveryLightOnTree () =
+        let christmasTree = ChristmasTree(1000,1000)
+        let turnedOnTree = christmasTree.TurnOnAll
+        for i in 0 .. turnedOnTree.Length - 1 do
+            Assert.IsTrue(turnedOnTree.[i] = true)
--- ChristmasTree/ChristmasTree.fs
+++ ChristmasTree/ChristmasTree.fs
@@ -3,7 +3,16 @@ module ChristmasTree
 type ChristmasTree(heightIn : int, widthIn : int) as self =
     let height = heightIn
     let width = widthIn
-    let grid : seq<bool> = seq{ for _row in 0 .. width - 1 do
-                                for _col in 0 .. height - 1 -> false }
+    let mutable christmasTree = [| for _i in 0 .. width - 1 do
+                                   for _j in 0 .. height - 1 -> false|]
     
-    member this.IsLightsOff = grid |> Seq.exists (fun i -> i  = false)
+    member this.IsLightsOff = christmasTree |> Array.exists (fun i -> i  = false)
+
+    member this.GetTree with get() =
+        christmasTree
+        
+    member this.TurnOnAll =
+        for i in 0 .. christmasTree.Length - 1 do
+            Array.set christmasTree i true
+        christmasTree
+                        
