06e4f37c5f080895634f6a90a791e81ad933e6cf
stanislav <s.lebel@dodopizza.com>
2021-10-05 16:36:59 +0300

    Complete first test on F#

--- ChristmasTree/ChristmasTree.fs
+++ ChristmasTree/ChristmasTree.fs
@@ -6,5 +6,4 @@ type ChristmasTree(heightIn : int, widthIn : int) as self =
     let grid : seq<bool> = seq{ for _row in 0 .. width - 1 do
                                 for _col in 0 .. height - 1 -> false }
     
-    member this.IsLightsOff() : bool = grid |> Seq.exists (fun i -> i = false)
-        
+    member this.IsLightsOff = grid |> Seq.exists (fun i -> i  = false)
